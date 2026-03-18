04811d6e5b40ea9e99917f3b0dc13a96071b12fa
admin <gittea@mail.com>
2026-03-18 16:19:35 +0100

    homepage

--- website/pages/homepage.py
+++ website/pages/homepage.py
@@ -6,7 +6,7 @@ dash.register_page(__name__, name="Home")
 layout = html.Div([
     html.H1("Movie Data Science Project"),
     
-    html.H2("Summary"),
+    html.H2("Intro"),
     html.P(
         "Movies are a major part of popular culture, and understanding what drives their success is both interesting and challenging. "
         "In this project, we explore factors influencing financial and critical outcomes of films, including production budgets, genres, release years, "
@@ -24,7 +24,6 @@ layout = html.Div([
         html.Li("How does the historical success over the last 30-40 years of the 10 most popular active directors influence the financial and critical success of their new movie releases?"),
         html.Li("How does the financial and critical success of movie sequels between 2010 and 2024 compare to their original films across franchises?"),
         html.Li("How does film genre, along with production budget, influence the likelihood of receiving an Academy Award nomination?"),
-        html.Li("How does receiving an Academy Award nomination or win influence a film’s box office revenue?"),
         html.Li("How does pre- and post-release Google search interest relate to the financial and critical success of movies released between 2010 and 2024?"),
         html.Li("How does sticking to typical genre plots lead to greater box office success for fantasy and horror movies (from 2000 to 2025)?")
     ]),
